c8855a723d61c6b8626e468fbbb11a3d32cb479b
SWMatte <peirettimatteo1@gmail.com>
2023-12-20 17:48:10 +0100

    UserEntity created

--- src/main/java/com/taskManagmentSystem/model/User.java
+++ src/main/java/com/taskManagmentSystem/model/User.java
@@ -1,4 +1,22 @@
 package com.taskManagmentSystem.model;
 
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUser;
+    private String name;
+    private String lastName;
+    private int age;
+    private LocalDate creationDate;
 }
