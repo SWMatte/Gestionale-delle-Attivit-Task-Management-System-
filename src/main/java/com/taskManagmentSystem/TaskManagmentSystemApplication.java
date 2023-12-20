8e3a7cc99ed9f0e5cf53f3f502be0967ab0729c2
SWMatte <peirettimatteo1@gmail.com>
2023-12-20 14:54:39 +0100

    first commit

---
+++ src/main/java/com/taskManagmentSystem/TaskManagmentSystemApplication.java
@@ -0,0 +1,18 @@
+package com.taskManagmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagmentSystemApplication.class, args);
+
+
+
+
+
+	}
+
+}
---
+++ src/test/java/com/taskManagmentSystem/TaskManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskManagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
